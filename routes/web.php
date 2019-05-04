05a802308cc7f6901a8c94a1349c2d1f43428b3f
Kelvin <liguekelvin@gmail.com>
2019-05-04 16:33:03 +0800

    add adminer

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::any('superadmin/db', '\Miroc\LaravelAdminer\AdminerAutologinController@index');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
