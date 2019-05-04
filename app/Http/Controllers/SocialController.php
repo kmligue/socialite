54c7f03bac3c3b86f2ea4b0efa509f3a544b9c15
Kelvin <liguekelvin@gmail.com>
2019-05-04 16:14:09 +0800

    facebook login

---
+++ app/Http/Controllers/SocialController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Socialite;
+use Auth;
+use App\User;
+
+class SocialController extends Controller
+{
+    public function redirect($provider)
+    {
+        return Socialite::driver($provider)->redirect();
+    }
+
+    public function callback($provider)
+    {
+        $userSocial = Socialite::driver($provider)->stateless()->user();
+        $users = User::where(['email' => $userSocial->getEmail()])->first();
+
+        if ($users) {
+            Auth::login($users);
+            return redirect('/');
+        } else {
+            $user = User::create([
+                'name'          => $userSocial->getName(),
+                'email'         => $userSocial->getEmail(),
+                'provider_id'   => $userSocial->getId(),
+                'provider'      => $provider,
+            ]);
+            return redirect()->route('home');
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('login/{provider}', 'SocialController@redirect');
+Route::get('login/{provider}/callback','SocialController@callback');
