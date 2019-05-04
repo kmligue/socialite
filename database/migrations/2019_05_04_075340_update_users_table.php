66c8dc08fdd5a4687396b63a22b036265990a0c4
Kelvin <liguekelvin@gmail.com>
2019-05-04 15:59:19 +0800

    update users table

---
+++ database/migrations/2019_05_04_075340_update_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('password')->nullable()->change();
+            $table->string('provider')->after('password');
+            $table->string('provider_id')->after('provider');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('usrs', function (Blueprint $table) {
+            $table->dropColumn(['provider', 'provider_id']);
+            $table->string('password')->change();
+        });
+    }
+}
